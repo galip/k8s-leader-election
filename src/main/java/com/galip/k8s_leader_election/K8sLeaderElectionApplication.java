eb8a65317bb4f05391b121a68cd389650c6cd8b6
galip <galippala@gmail.com>
2026-02-09 00:46:42 +0300

    Initial commit for kubernetes leader election demo for order worker

---
+++ src/main/java/com/galip/k8s_leader_election/K8sLeaderElectionApplication.java
@@ -0,0 +1,13 @@
+package com.galip.k8s_leader_election;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sLeaderElectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sLeaderElectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galip/k8s_leader_election/K8sLeaderElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galip.k8s_leader_election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sLeaderElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
